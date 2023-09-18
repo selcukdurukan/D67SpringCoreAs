ad07916fe51b6be33ec1948fa828f5e65ccee576
selcukdurukan <selcukdurukan@outlook.com.tr>
2023-09-18 20:46:21 +0300

    The second commit

--- src/main/java/com/ba/boost/example1/Main.java
+++ src/main/java/com/ba/boost/example1/Main.java
@@ -6,7 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
-
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("services.xml");
+        applicationContext.getBean(RunService.class).run();
 
     }
 }
